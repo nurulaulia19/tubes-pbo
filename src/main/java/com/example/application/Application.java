f717bf9753b7f62b01b69f66e0a02dc8dccb5eed
nurulaulia19 <nurulauliaseptiani9@gmail.com>
2023-01-07 18:52:22 +0700

    memperbaiki program application.java

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -11,13 +11,7 @@ import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDataba
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
 import org.springframework.context.annotation.Bean;
 
-/**
- * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
- */
+
 @SpringBootApplication
 @Theme(value = "biodata")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
